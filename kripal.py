99c4a90ef4906e9ebd88c65a7c6366608e8819d2
MrBubby19 <83509841+MrBubby19@users.noreply.github.com>
2021-05-01 22:17:20 +0530

    Update kripal.py

--- kripal.py
+++ kripal.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip3 install git+https://github.com/thar2/titli.git")
-os.system("git clone https://github.com/rahulji765789021/murgi.git")
-os.system("chmod 777 murgi/ram1.py")
+os.system("pip3 install git+https://github.com/MrBubby19/patani.git")
+os.system("git clone https://github.com/MrBubby19/right.git")
+os.system("chmod 777 right/sonu.py")
 #os.system("python3 murgi/ram1.py &")
 
